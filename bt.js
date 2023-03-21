fa6291b201fea02868eaa0de720aa7b6be173739
hoang.le <leehoang@gmail.com>
2023-03-21 17:40:20 +0700

    done

---
+++ bt.js
@@ -0,0 +1,14 @@
+// Bai 8
+const maxBanhdanem = 9;
+let banhCanRan=15;
+let soLan = Math.floor(banhCanRan / maxBanhdanem);
+if (banhCanRan % maxBanhdanem !== 0) {
+    soLan++;
+} else {
+    soLan;
+}
+
+
+console.log(soLan);
+
+//Bai3
